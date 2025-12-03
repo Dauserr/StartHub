b8963c05fd82824b673ae95c14f1d8a770a6d387
olzhassembinov <o_sembinov@kbtu.kz>
2025-12-03 13:31:10 +0500

    ui change, screens and bottom navigation bar

---
+++ app/src/main/java/com/example/starthub/ui/screens/BottomNavItem.kt
@@ -0,0 +1,18 @@
+package com.example.starthub.ui.screens
+
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.ui.graphics.vector.ImageVector
+
+
+sealed class BottomNavItem(val route: String,
+                           val title: String,
+                           val icon: ImageVector) {
+    object Home : BottomNavItem("home", "Home", Icons.Default.Home)
+    object Catalogue : BottomNavItem("catalogue", "Catalogue", Icons.Default.ShoppingCart)
+    object Profile : BottomNavItem("profile", "Profile", Icons.Default.Person)
+
+}
