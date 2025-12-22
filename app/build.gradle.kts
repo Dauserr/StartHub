72250ef54f15b9fe4f4b7928916da0370ebfee3c
Dauserr <dauser200606@gmail.com>
2025-12-22 13:12:35 +0500

    Revert "Merge branch 'authscreen' into feature/authentication"
    This reverts commit a7ca284a8e7de7a8c1c805f2c12d86d2e85717b4, reversing
    changes made to 5e76f0cafa563634397ddc575f69119c5c948234.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("kotlin-kapt")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21"
 }
 
 android {
@@ -57,9 +56,6 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
 
-    //navigation compose
-    implementation(libs.androidx.navigation.compose)
-
     // Retrofit (API calls)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
@@ -78,25 +74,10 @@ dependencies {
     // Room Database
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.ui)
     kapt(libs.androidx.room.compiler)
 
     // RecyclerView (for catalogue list)
     implementation(libs.androidx.recyclerview)
-
-    //compose
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material.icons.extended)
-
-
-    //compose debug
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     testImplementation(libs.junit)
 
